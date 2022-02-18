8ca41b6ab2a5a231c181678950b3bb82830fb5c8
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-18 20:58:49 +0530

    Added : Welcome to Address Book

--- src/com/address/addressBook.java
+++ src/com/address/addressBook.java
@@ -1,20 +1,8 @@
 package com.address;
 
-class contacts {
-	private String firstName;
-	private String lastName;
-	private String address;
-	private String city;
-	private int zipcode;
-	private String state;
-	private int phoneNo;
-	private String email;
-
-}
-
 public class addressBook {
-
 	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Program in AddressBook Main class");
+		System.out.println("Welcome to Address Book Program in AddressBookMain class");
 	}
+
 }
