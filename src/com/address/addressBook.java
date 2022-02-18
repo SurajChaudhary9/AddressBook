347ea32a0309fd4673209a6afa50d1155fc0e572
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-18 21:12:27 +0530

    UC1 Added : Ability to create contacts in adress book

--- src/com/address/addressBook.java
+++ src/com/address/addressBook.java
@@ -2,7 +2,7 @@ package com.address;
 
 public class addressBook {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Program in AddressBookMain class");
+		System.out.println("Welcome to Address Book Program in AddressBook Main class");
 	}
 
 }
