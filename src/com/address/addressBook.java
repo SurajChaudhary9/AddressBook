3a4178c709fc21e1871a8d12c13e767d3706dae9
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-20 02:30:26 +0530

    Added : Ability to Delete contact Using Person Name

--- src/com/address/addressBook.java
+++ src/com/address/addressBook.java
@@ -1,12 +1,12 @@
 package com.address;
-/*Ability to edit existing contact person using their name
- * Using console to edit person details
+/*Ability to delete a contact from address book
+ * Using console to delete a contact
  */
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class addressBook {
+public class addressBook {
 	static Scanner scanner = new Scanner(System.in);
 	ArrayList<Contacts> person;
 
@@ -16,7 +16,6 @@ class addressBook {
 	}
 
 	// add new person record to array list after taking input
-
 	public Contacts addPerson() {
 		System.out.println("Enter the First Name");
 		String firstName = scanner.next();
@@ -36,40 +35,52 @@ class addressBook {
 		String emailId = scanner.next();
 		// creating constructor
 		Contacts contacts = new Contacts(firstName, lastName, address, city, state, zip, phoneNumber, emailId);
-		// adding the above list to to Contacts array list
+		// add the above list to to Contacts array list
 		person.add(contacts);
-		// printing the contacts object data inside
+		// printing contacts object data inside
 		System.out.println(contacts);
-
 		return contacts;
-
 	}// end of addPerson method
+		// Editing contact details using person's name
 
-	// Editing contact details using person's name
 	public void editPerson() {
 		System.out.println("Enter name to Edit");
 		String s = scanner.next();
 		addressBook ab = new addressBook();
+		for (int i = 0; i < person.size(); i++) {
+			Contacts p = (Contacts) person.get(i);
+			if (s.equals(p.getFirstName())) {
+				System.out.println(p);
+				p = ab.addPerson();/// calling add person to replace
+				person.set(i, p);
+				System.out.println(p);
+			}
+		}
+	}
+
+	// deleting contact details using person's name
+	public void deletePerson() {
+		System.out.println("Enter name to Delete");
+		String s = scanner.next();
 
 		for (int i = 0; i < person.size(); i++) {
 			Contacts p = (Contacts) person.get(i);
 			if (s.equals(p.getFirstName())) {
 				System.out.println(p);
-				p = ab.addPerson();/// calling add person to replace initial i/p
-
-				for (int j = 0; j < person.size(); j++) {
-					person.set(j, p);
-				}
-
+				person.remove(i);
+				System.out.println(person.isEmpty());
 			}
 		}
+
 	}
 
+	// main program
 	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Program in AddressBookMain class");
+		System.out.println("Welcome to Address Book Program in AddressBook Main class");
 		addressBook ab = new addressBook();
 		ab.addPerson();
 		ab.editPerson();
+		ab.deletePerson();
 
 	}// main end
 
