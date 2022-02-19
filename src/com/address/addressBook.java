7b33cb3742dd37833dac44f4358fc48a20aa9a37
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-20 01:41:11 +0530

    Added : Create a Address Book

--- src/com/address/addressBook.java
+++ src/com/address/addressBook.java
@@ -1,8 +1,102 @@
 package com.address;
+/*Ability to create a Contacts in Address
+Book with first and last names, address,
+city, state, zip, phone number and
+email...
+ */
 
-public class addressBook {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Program in AddressBook Main class");
+class addressBook {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNumber;
+	private String email;
+
+	// constructor addressbook
+	public addressBook(String firstName, String lastName, String address, String city, String state, String zip,
+			String phoneNumber, String email) {
+		setFirstName(firstName);
+		setLastName(lastName);
+		setAddress(address);
+		setCity(city);
+		setState(state);
+		setZip(zip);
+		setPhoneNumber(phoneNumber);
+		setEmail(email);
 	}
 
-}
+	// getters setters for parameters
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String toString() {
+		return "Contacts [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+				+ ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email + "]";
+	}
+
+}/// end of class
